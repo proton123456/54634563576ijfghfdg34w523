59043bbb5e70e9b39c6045a19b545ac949fd7433
TB - roblox scripter <101665024+proton123456@users.noreply.github.com>
2022-11-10 20:32:00 -0500

    Update bypassKeyCheckpoints.py

--- bypassKeyCheckpoints.py
+++ bypassKeyCheckpoints.py
@@ -1,10 +1,8 @@
 
-checkpoints = 5 # amount of checkpoints/linkvertise ads
-HardwareCurrentKey = "" # generates when the bot finishes the key 
+checkpoints = int(0) # amount of checkpoints/linkvertise ads
+HardwareCurrentKey = str("nil") # generates when the bot finishes the key 
 
 
 # krnl bypasser
-
-
 
 
